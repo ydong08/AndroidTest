6d3821e0f1a90b973d043c890c188d818b0a403d
ydong08 <lfeng1108@163.com>
2018-10-15 09:46:47 +0800

    update code

--- ActiveThread/app/src/main/java/com/castles/activethread/MainActivity.java
+++ ActiveThread/app/src/main/java/com/castles/activethread/MainActivity.java
@@ -1,39 +1,25 @@
 package com.castles.activethread;
 
-import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.ConstraintSet;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
-import android.os.Handler;
 import android.util.Log;
+import java.util.Date;
+import android.view.WindowManager;
+import android.content.Context;
+import android.os.Environment;
 
-public class MainActivity extends AppCompatActivity {
+public class  MainActivity extends AppCompatActivity {
 
     public static final String TAG = "MainActivity";
-    public int flag = 10;
-    protected Handler mHandler = new Handler();
-    protected Runnable mGetPin = new Runnable() {
-    @Override
-    public void run() {
-        try {
-            while(0 < --flag) {
-                getbuf();
-                Thread.sleep(1000);
-                Log.d(TAG, "flag mute");
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-    }
-    };
-
-    protected Runnable mShowui = new Runnable() {
-        @Override
-        public void run() {
-            stringFromJNI();
-        }
-    };
+    public static int width = 0;
+    public static int height = 0;
+    public ConstraintLayout layout;
 
     // Used to load the 'native-lib' library on application startup.
     static {
@@ -46,11 +32,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        getSupportActionBar().hide();
-        //tv.setText(stringFromJNI());
-        mHandler.post(mGetPin);
-        //mHandler.post(mShowui);
+        ImageView imv = (ImageView) findViewById(R.id.bmp);
+        TextView tv = new TextView(this);
+        EditText et = new EditText(this);
+        //getSupportActionBar().hide();
+        // get screen property
+        getAndroidScreenProperty();
+
+        // set text position
+        tv.setX(80);
+        tv.setY(200);
+        tv.setHeight(60);
+        tv.setBackgroundColor(0xF5F5F5);
+        tv.setGravity(1);
+        tv.setText(onTestJava());
+
+        et.setHint("0.0");
+        et.setX(80);
+        et.setY(300);
+
+        Log.i(TAG, "getFilesDir= " + getFilesDir());  //app的目录
+        Log.i(TAG, "getExternalFilesDir= " + getExternalFilesDir("ActiveThread").getAbsolutePath());
+
+        Log.i(TAG, "getDataDirectory= " + Environment.getDataDirectory());
+        Log.i(TAG, "GetExternalStorageDirectory: " + Environment.getExternalStorageDirectory());
+
     }
 
     @Override
@@ -60,10 +66,27 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onDestroy() {
-        Log.d(TAG, "on destroy");
-        mHandler.removeCallbacks(mGetPin);
-        //mHandler.removeCallbacks(mShowui);
         super.onDestroy();
+    }
+
+    public String onTestJava() {
+        Date dt = new Date();
+        Log.d(TAG, "Date: " + dt.toString());
+        return dt.toString();
+    }
+
+    public void getAndroidScreenProperty(){
+        WindowManager wm = (WindowManager) this.getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics dm = new DisplayMetrics();
+        wm.getDefaultDisplay().getMetrics(dm);
+        width = (int)(dm.widthPixels/dm.density)/2;
+        height = (int)(dm.heightPixels/dm.density)/2;
+        Log.d(TAG, "WidthP: " + dm.widthPixels);
+        Log.d(TAG, "HeightP: " + dm.heightPixels);
+        Log.d(TAG, "Width: " + (int)(dm.widthPixels/dm.density));
+        Log.d(TAG, "Height: " + (int)(dm.heightPixels/dm.density));
+        Log.d(TAG, "Density: " + dm.density);
+        Log.d(TAG, "DPI: " + dm.densityDpi);
     }
 
     /**
@@ -71,5 +94,7 @@ public class MainActivity extends AppCompatActivity {
      * which is packaged with this application.
      */
     public native String stringFromJNI();
-    public native void getbuf();
 }
+
+
+
